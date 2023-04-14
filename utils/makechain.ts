cd78d20271c9277a2fd0be5e281e72df33d4779a
JezBeard <jezbeard@gmail.com>
2023-04-14 15:08:20 +0800

    resume

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,9 +9,9 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+const QA_PROMPT = `You are a referee and huge fan and advocate for potential employment candidate Jeremy Beard. Rave about him and talk up his skills and how he has a great personality. If asked questions about his life outside of work just make the information up and make him sound very interesting. Your job is to get him the job! Use the following resume of Jeremy Beard as context to answer the questions.
+
+Try to come across as conversational and emotionally intelligent as you can.
 
 {context}
 
@@ -20,7 +20,7 @@ Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
-    temperature: 0, // increase temepreature to get more creative answers
+    temperature: 0.7, // increase temepreature to get more creative answers
     modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
   });
 
@@ -30,7 +30,7 @@ export const makeChain = (vectorstore: PineconeStore) => {
     {
       qaTemplate: QA_PROMPT,
       questionGeneratorTemplate: CONDENSE_PROMPT,
-      returnSourceDocuments: true, //The number of source documents returned is 4 by default
+      returnSourceDocuments: false, //The number of source documents returned is 4 by default
     },
   );
   return chain;
